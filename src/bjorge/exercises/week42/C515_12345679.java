8920f41cefdc81773384f0f304d77346861e6faa
Bjurge <bjorge.s.djuve@hotmail.com>
2021-10-22 15:02:46 +0200

    JObbet fra til 5.1.6 8

--- src/bjorge/exercises/week42/C515_12345679.java
+++ src/bjorge/exercises/week42/C515_12345679.java
@@ -1,4 +1,72 @@
 package bjorge.exercises.week42;
 
+import bjorge.structure.BinTre;
+
+// 5.1.5  Datarepresentasjon av noder og binære trær
 public class C515_12345679 {
+    public static void main(String[] args) {
+        int[] posisjon = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,17,18,19,21,23,26,27,29};
+        Character[] verdi = {'A','B','C','D','E','F','G','H','I','J','K',
+                'L','M','N','O','P','Q','R','S','T','U','V'};
+
+        BinTre<Character> tre = new BinTre<>(posisjon, verdi);  // den nye konstruktøren
+        // Oppgave 2
+        int[] posisjon1 = {1,2,3,5,6,7,10,11,12,13,21,24,25,42,43};
+        Character[] verdi1 = {'D','I','H','L','O','B','A','E','N','G','K','M','J','F','C'};
+
+        BinTre<Character> tre1 = new BinTre<>(posisjon1, verdi1);
+
+        // Oppgave 5
+        BinTre<Integer> tre5 = new BinTre<>();
+
+        int n = 15;
+        for (int i = 1; i <= n; i++) tre5.leggInn(i,i);
+
+        // Oppgave 6
+        // Kan lages på denne noe tungvinte måten:
+
+        int[] p = {1,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,16384};
+        Integer[] v = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+
+        BinTre<Integer> tre6 = new BinTre<>(p,v);
+
+
+        // Dette er en enklere og mer generell måte:
+
+        BinTre<Integer> tre6b = new BinTre<>();
+
+        n = 15;
+        int k = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            tre6b.leggInn(k,i);
+            k *= 2;
+        }
+        // Oppgave 7
+        // Kan lages på denne måten:
+
+        int[] p7 = {1,2,3,4,7,8,15,16,31,32,63,64,127,128,255};
+        Integer[] v7 = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+
+        BinTre<Integer> tre7 = new BinTre<>(p7,v7);
+
+
+        // Dette er en mer generell måte:
+
+        BinTre<Integer> tre7g = new BinTre<>();
+
+        tre7g.leggInn(1,1);    // lager rotnoden
+
+        n = 7;
+        k = 2; // starter å posisjon 2
+
+        for (int i = 1; i <= n; i++)
+        {
+            tre7g.leggInn(k,2*i);
+            tre7g.leggInn(2*k-1,2*i+1);
+            k *= 2;
+        }
+
+
+    }
 }
--- src/bjorge/exercises/week42/D516_1345810.java
+++ src/bjorge/exercises/week42/D516_1345810.java
@@ -1,4 +1,75 @@
 package bjorge.exercises.week42;
 
+import bjorge.eksempelKlasser.Liste;
+import bjorge.eksempelKlasser.Oppgave;
+import bjorge.eksempelKlasser.TabellListe;
+import bjorge.structure.BinTre;
+import bjorge.structure.Tabell;
+
+import java.util.Arrays;
+import java.util.StringJoiner;
+
 public class D516_1345810 {
+    public static void main(String[] args) {
+        /*
+        int[] posisjon = {1,2,3,4,5,6,7,10,11,13,14,22,23,28,29};  // posisjoner og
+        String[] verdi = "EIBGAHKLODNMCJF".split("");              // verdier i nivåorden
+
+        BinTre<String> tre = new BinTre<>(posisjon, verdi);        // en konstruktør
+        tre.nivåorden(c -> System.out.print(c + " "));  // lambda-uttrykk som argument
+        // Utskrift: E I B G A H K L O D N M C J F
+
+        Liste<Character> liste = new TabellListe<>();  // en liste
+        tre.nivåorden(c -> liste.leggInn(c));          // lambda-uttrykk som argument
+        System.out.println(liste);                     // skriver ut listen
+
+
+        int[] posisjon = {1,2,3,4,5,6,7,10,11,13,14,22,23,28,29};  // nodeposisjoner
+        String[] verdi = "EIBGAHKLODNMCJF".split("");              // verdier i nivåorden
+
+        BinTre<String> tre = new BinTre<>(posisjon, verdi);        // en konstruktør
+
+        int[] nivåer = tre.nivåer();  // bruker Programkode 5.1.6 k)
+
+        System.out.print("Nivåer: " + Arrays.toString(nivåer));
+        System.out.print(" Treets bredde: " + nivåer[Tabell.maks(nivåer)]);
+        System.out.println(" Treets høyde: " + (nivåer.length - 1));
+
+        // Utskrift: Nivåer: [1, 2, 4, 4, 4] Treets bredde: 4 Treets høyde: 4
+
+        // Oppgave 3
+        tre.nivåorden(Oppgave.konsollutskrift().deretter(c -> System.out.print(' ')));
+        // Oppgave 4
+        BinTre<Character> tre4 = new BinTre<>();             // et tomt tre
+
+        int[] p = {1,2,3,4,5,6,7,10,11,13,14,22,23,28,29};  // posisjoner i nivåorden
+        String v = "EIBGAHKLODNMCJF";                       // verdier i nivåorden
+        for (int i = 0; i < p.length; i++) tre4.leggInn(p[i],v.charAt(i));
+
+        StringJoiner s = new StringJoiner(", ", "[", "]");
+
+        tre.nivåorden(c -> s.add(c.toString()));
+
+        String verdier = s.toString();
+        System.out.println(verdier);                    // skriver ut listen
+        */
+        // Oppgave 5
+        BinTre<Character> tre5 = new BinTre<>();             // et tomt tre
+
+        int[] p5 = {1,2,3,4,5,6,7,10,11,13,14,22,23,28,29};  // posisjoner i nivåorden
+        String v5 = "EIBGAHKLODNMCJF";                       // verdier i nivåorden
+        for (int i = 0; i < p5.length; i++) tre5.leggInn(p5[i],v5.charAt(i));
+
+        StringJoiner s5 = new StringJoiner(", ", "[", "]");
+
+        char d = 'D';
+        tre5.nivåorden(c -> {
+            if (c > d) s5.add(c.toString());
+        } );
+
+        String verdier5 = s5.toString();
+        System.out.println(verdier5);                    // skriver ut listen
+        // Oppgave 8
+
+    }
 }
